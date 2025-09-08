78eb1ae03de4017ccd79eff311089afb229f70b6
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-08 13:35:16 +0100

    feat: criacao do bloco

--- script/variaveis do jogo.js
+++ script/variaveis do jogo.js
@@ -1,4 +1,4 @@
-var canvas, ctx, ALTURA, LARGURA, frames = 0;
+var canvas, ctx, ALTURA, LARGURA, frames = 0, maxPulos = 3,
 
 chao = {
     y: 550,
@@ -9,11 +9,47 @@ chao = {
         ctx.fillStyle = this.cor;
         ctx.fillRect(0, this.y, LARGURA, this.altura);
     }
+},
+
+bloco = {
+    x: 50,
+    y: 0,
+    altura: 50,
+    largura: 50,
+    cor: "#ff4e4e",
+    gravidade: 1.5,
+    velocidade: 0,
+    forcaDoPulo: 15,
+    qntPulos: 0,
+
+    atualiza: function(){
+        this.velocidade += this.gravidade;
+        this.y += this.velocidade;
+
+        // Impede que o bloco caia abaixo do chao
+        if(this.y > chao.y - this.altura){
+            this.y = chao.y - this.altura;
+            this.qntPulos = 0;
+        }
+    },
+
+    pula: function(){
+        // Impede que o bloco pule mais que o maximo de pulos
+        if(this.qntPulos < maxPulos){
+            this.velocidade = -this.forcaDoPulo;
+            this.qntPulos++;
+        }
+    },
+
+    desenha: function(){
+        ctx.fillStyle = this.cor;
+        ctx.fillRect(this.x, this.y, this.largura, this.altura);
+    }
 };
 
 
 function clique(event){
-    alert("Clique");
+    bloco.pula();
 }
 
 // Função principal do jogo
@@ -45,12 +81,15 @@ function roda(){
 }
 function atualiza(){ // Atualiza a lógica do jogo
     frames++;
+
+    bloco.atualiza();
 } 
 function desenha(){ // Desenha na tela
     ctx.fillStyle = "#50beff"; // Define a cor do fundo
     ctx.fillRect(0, 0, LARGURA, ALTURA); // Desenha o fundo
 
     chao.desenha();
+    bloco.desenha();
 } 
 
 // Inicia o jogo
