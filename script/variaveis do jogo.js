b8203a7e12959098a9a435ea9846e419a1163d4e
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-02 19:53:40 +0100

    update jogo

--- script/variaveis do jogo.js
+++ script/variaveis do jogo.js
@@ -1,131 +1,142 @@
 //  Defindo as variaveis do jogo
 var canvas, ctx, ALTURA, LARGURA, frames = 0, maxPulos = 3, velocidade = 6, estadoAtual,
 
-// Estados do jogo
-estados = {
-    jogar: 0,
-    jogando: 1,
-    perdeu: 2
-},
-
-// Objeto chao
-chao = {
-    y: 550,
-    altura: 50,
-    cor: "#c8da78",
-
-    desenha: function(){
-        ctx.fillStyle = this.cor;
-        ctx.fillRect(0, this.y, LARGURA, this.altura);
-    }
-},
-
-// Objeto bloco
-bloco = {
-    x: 50,
-    y: 0,
-    altura: 50,
-    largura: 50,
-    cor: "#ff9239",
-    gravidade: 1.6,
-    velocidade: 0,
-    forcaDoPulo: 23.6,
-    qntPulos: 0,
-    // Atualiza a posicao do bloco
-    atualiza: function(){
-        this.velocidade += this.gravidade;
-        this.y += this.velocidade;
-
-        // Impede que o bloco caia abaixo do chao
-        if(this.y > chao.y - this.altura){
-            this.y = chao.y - this.altura;
-            this.qntPulos = 0;
-        }
+    // Estados do jogo
+    estados = {
+        jogar: 0,
+        jogando: 1,
+        perdeu: 2
     },
-    // Faz o bloco pular
-    pula: function(){
-        // Impede que o bloco pule mais que o maximo de pulos
-        if(this.qntPulos < maxPulos){
-            this.velocidade = -this.forcaDoPulo;
-            this.qntPulos++;
+
+    // Objeto chao
+    chao = {
+        y: 550,
+        altura: 50,
+        cor: "#c8da78",
+
+        desenha: function () {
+            ctx.fillStyle = this.cor;
+            ctx.fillRect(0, this.y, LARGURA, this.altura);
         }
     },
 
-    // Desenha o bloco na tela
-    desenha: function(){
-        ctx.fillStyle = this.cor;
-        ctx.fillRect(this.x, this.y, this.largura, this.altura);
-    }
-},
+    // Objeto bloco
+    bloco = {
+        x: 50,
+        y: 0,
+        altura: 50,
+        largura: 50,
+        cor: "#ff9239",
+        gravidade: 1.6,
+        velocidade: 0,
+        forcaDoPulo: 23.6,
+        qntPulos: 0,
+        // Atualiza a posicao do bloco
+        atualiza: function () {
+            this.velocidade += this.gravidade;
+            this.y += this.velocidade;
 
-// Objeto obstaculos
-obstaculos = {
-    _obs: [],
-    cores: ["#ffbc1c", "#ff1c1c", "#ff85e1", "#52a7ff", "#78ff5d"],
-    tempoInsere: 0,
+            // Impede que o bloco caia abaixo do chao
+            if (this.y > chao.y - this.altura && estadoAtual != estados.perdeu) {
+                this.y = chao.y - this.altura;
+                this.qntPulos = 0;
+                this.velocidade = 0;
+            }
+        },
+        // Faz o bloco pular
+        pula: function () {
+            // Impede que o bloco pule mais que o maximo de pulos
+            if (this.qntPulos < maxPulos) {
+                this.velocidade = -this.forcaDoPulo;
+                this.qntPulos++;
+            }
+        },
 
-    insere: function(){
-        this._obs.push({
-            x: LARGURA,
-            largura: 30 + Math.floor(21 * Math.random()),
-            altura: 30 + Math.floor(120 * Math.random()),
-            cor: this.cores[Math.floor(5 * Math.random())]
-        });
-        this.tempoInsere = 30 + Math.floor(21 * Math.random());
+        // Desenha o bloco na tela
+        desenha: function () {
+            ctx.fillStyle = this.cor;
+            ctx.fillRect(this.x, this.y, this.largura, this.altura);
+        }
     },
 
-    // Atualiza a posicao dos obstaculos
-    atualiza: function(){  
-        // Veifica se eh tempo de inserir um novo obstaculo
-        if(this.tempoInsere == 0){
-            this.insere();
-        }else{
-            this.tempoInsere--;
-        }
+    // Objeto obstaculos
+    obstaculos = {
+        _obs: [],
+        cores: ["#ffbc1c", "#ff1c1c", "#ff85e1", "#52a7ff", "#78ff5d"],
+        tempoInsere: 0,
+
+        insere: function () {
+            this._obs.push({
+                x: LARGURA,
+                largura: 30 + Math.floor(21 * Math.random()),
+                altura: 30 + Math.floor(120 * Math.random()),
+                cor: this.cores[Math.floor(5 * Math.random())]
+            });
+            this.tempoInsere = 30 + Math.floor(21 * Math.random());
+        },
+
+        // Atualiza a posicao dos obstaculos
+        atualiza: function () {
+            // Veifica se eh tempo de inserir um novo obstaculo
+            if (this.tempoInsere == 0)
+                this.insere();
+            else
+                this.tempoInsere--;
 
 
-        for(var i = 0, tam = this._obs.length; i < tam; i++){
-            var obs = this._obs[i];
 
-            obs.x -= velocidade;
+            for (var i = 0, tam = this._obs.length; i < tam; i++) {
+                var obs = this._obs[i];
 
-            if(obs.x <= -obs.largura){
-                this._obs.splice(i, 1);
-                tam--;
-                i--;
+                obs.x -= velocidade;
+
+                if (bloco.x < obs.x + obs.largura && bloco.x + bloco.largura >= obs.x && bloco.y + bloco.altura >= chao.y - obs.altura) {
+                    estadoAtual = estados.perdeu;
+                }
+
+                else if (obs.x <= -obs.largura) {
+                    this._obs.splice(i, 1);
+                    tam--;
+                    i--;
+                }
+            }
+        },
+
+        limpa: function () {
+            this._obs = [];
+        },
+
+        desenha: function () {
+            for (var i = 0, tam = this._obs.length; i < tam; i++) {
+                var obs = this._obs[i];
+                ctx.fillStyle = obs.cor;
+                ctx.fillRect(obs.x, chao.y - obs.altura, obs.largura, obs.altura);
             }
         }
-    },
-
-    desenha: function(){
-        for(var i = 0, tam = this._obs.length; i < tam; i++){
-            var obs = this._obs[i];
-            ctx.fillStyle = obs.cor;
-            ctx.fillRect(obs.x, chao.y - obs.altura, obs.largura, obs.altura);
-        }
-    }
-};
+    };
 
 // Função que trata o clique do mouse
-function clique(event){
-    if(estadoAtual == estados.jogando)
+function clique(event) {
+    if (estadoAtual == estados.jogando)
         bloco.pula();
 
-    else if(estadoAtual == estados.jogar){
+    else if (estadoAtual == estados.jogar) {
         estadoAtual = estados.jogando;
     }
-    
-    else if(estadoAtual == estados.perdeu){
+
+    else if (estadoAtual == estados.perdeu && bloco.y >= 2 * ALTURA) {
         estadoAtual = estados.jogar;
+        bloco.velocidade = 0;
+        bloco.y = 0;
     }
 }
 
 // Função principal do jogo
-function main(){
+function main() {
     ALTURA = window.innerHeight; // Devovlve o tamanho da janela do usuario
     LARGURA = window.innerHeight; // Devovlve o tamanho da janela do usuario
 
-    if(LARGURA >= 500){ // Forcando a largura e altura a ter tamanho fixo
+    if (LARGURA >= 500) { // Forcando a largura e altura a ter tamanho fixo
         LARGURA = 600;
         ALTURA = 600;
     }
@@ -144,44 +155,48 @@ function main(){
 }
 
 // Função que roda o jogo
-function roda(){
+function roda() {
     atualiza();
     desenha();
 
     // Chama a função roda novamente
-    window.requestAnimationFrame(roda); 
+    window.requestAnimationFrame(roda);
 }
 
 // Atualiza a lógica do jogo
-function atualiza(){ 
+function atualiza() {
     frames++;
 
     bloco.atualiza();
-    if(estadoAtual == estados.jogando)
+
+    if (estadoAtual == estados.jogando)
         obstaculos.atualiza();
-} 
+
+    else if (estadoAtual == estados.perdeu)
+        obstaculos.limpa();
+}
 
 //  Funcao que Desenha na tela
-function desenha(){ 
+function desenha() {
     ctx.fillStyle = "#80daff"; // Define a cor do fundo
     ctx.fillRect(0, 0, LARGURA, ALTURA); // Desenha o fundo
 
-    if(estadoAtual == estados.jogar){
+    if (estadoAtual == estados.jogar) {
         ctx.fillStyle = "green";
         ctx.fillRect = (LARGURA / 2 - 50, ALTURA / 2 - 50, 100, 100);
-    } 
+    }
 
-    else if(estadoAtual == estados.perdeu){
+    else if (estadoAtual == estados.perdeu) {
         ctx.fillStyle = "red";
         ctx.fillRect = (LARGURA / 2 - 50, ALTURA / 2 - 50, 100, 100);
     }
 
-    else if(estadoAtual == estados.jogando)
+    else if (estadoAtual == estados.jogando)
         obstaculos.desenha();
 
     chao.desenha();
     bloco.desenha();
-} 
+}
 
 // Inicia o jogo funcao principal do jogo
 main();
